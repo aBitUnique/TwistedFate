92c72274202448f372ae3993be1dfc2e1e287025
aBitUnique <42247195+aBitUnique@users.noreply.github.com>
2021-05-07 21:14:30 +0100

    Added in champion and mastery endpoints plus ddragon champion data

--- league_test.go
+++ league_test.go
@@ -1,15 +1,15 @@
-package test
+package TwistedFate
 
 import (
-	TF "TwistedFate"
 	"fmt"
+	TF "github.com/aBitUnique/TwistedFate"
 	"testing"
 )
 
 func TestGetLeagueEntryByAccountId(t *testing.T) {
 	s, _ := TF.GetLeagueEntryBySummonerId("97Vtefw0C2x_UtDXe94THK1kwGivO8ZhBZsDmXxIQXS676I")
 	fmt.Println((*s)[0])
-	if (*s)[0].SummonerName != "Gwal66" {
-		t.Errorf("Should be user Gwal66 but got %s", (*s)[0].SummonerName)
+	if s[0].SummonerName != "Gwal66" {
+		t.Errorf("Should be user Gwal66 but got %s", s.SummonerName)
 	}
 }
